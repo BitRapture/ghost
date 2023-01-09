d4e3f9dc5911e0773773d1813fc713537ad1fe8c
BitRapture <62811797+BitRapture@users.noreply.github.com>
2023-01-09 23:06:04 +0000

    Add slash

--- Discord/src/bot.js
+++ Discord/src/bot.js
@@ -12,11 +12,11 @@ const Bot = {
     }
 }
 
-const slashCommandPath = __dirname + "commands/slash";
+const slashCommandPath = __dirname + "/commands/slash";
 var slashCommandFiles = FS.readdirSync(slashCommandPath).filter(file => file.endsWith(".js"));
 for (var file of slashCommandFiles) { file = `${slashCommandPath}${file}` }
 
-const plainCommandPath = __dirname + "commands/plain";
+const plainCommandPath = __dirname + "/commands/plain";
 var plainCommandFiles = FS.readdirSync(plainCommandPath).filter(file => file.endsWith(".js"));
 for (var file of plainCommandFiles) { file = `${plainCommandPath}${file}` }
 
