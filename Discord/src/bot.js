dde087e52457dcf933d4d2c85d8be10d22362568
BitRapture <62811797+BitRapture@users.noreply.github.com>
2023-01-09 23:23:28 +0000

    Add slash command syncing

--- Discord/src/bot.js
+++ Discord/src/bot.js
@@ -1,5 +1,5 @@
 const FS = require("node:fs");
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, REST, Routes } = require("discord.js");
 // Create structure
 const Bot = {
     Client : new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] }),
@@ -11,7 +11,9 @@ const Bot = {
         Slash : new Map()
     }
 }
+const Rest = new REST({ version: "10" }).setToken(Bot.Config.Tokens.discord);
 
+// Accumulate all the commands
 const slashCommandPath = __dirname + "/commands/slash";
 var slashCommandFiles = FS.readdirSync(slashCommandPath).filter(file => file.endsWith(".js"));
 slashCommandFiles.forEach(file => { file = `${slashCommandPath}${file}` });
@@ -20,24 +22,48 @@ const plainCommandPath = __dirname + "/commands/plain";
 var plainCommandFiles = FS.readdirSync(plainCommandPath).filter(file => file.endsWith(".js"));
 plainCommandFiles.forEach(file => { file = `${plainCommandPath}${file}` });
 
-const allCommandFiles = slashCommandFiles.concat(plainCommandFiles)
+const allCommandFiles = slashCommandFiles.concat(plainCommandFiles);
 
+// Save to bot struct
 allCommandFiles.forEach(commandFile => {
     const command = require(commandFile);
     // Inspect if the command is a slash command
     if ("data" in command && "execute" in command) {
         Bot.Commands.Slash.set(command.data.name, command);
+    // Otherwise add to plain text command table
     } else if ("execute" in command) {
         Bot.Commands.Plain.set(command.data.name, command);
     }
 });
 
+// Sync slash commands
+const serializedSlashCommands = [];
+Bot.Commands.Slash.forEach(command => {
+    serializedSlashCommands.push(command.data.toJSON());
+});
+(async () => {
+    try {
+        console.log("GHOST > Syncing slash commands");
+
+        const data = await Rest.put(
+            Routes.applicationCommands(Bot.Config.Tokens.discordId),
+            { body: serializedSlashCommands }
+        );
+
+        console.log(`GHOST > Successfully synced ${data.length} slash command(s)`);
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+// Discord bot online
 Bot.Client.on("ready", () => {
     console.log(`GHOST > Logged in as: ${Bot.Client.user.tag}`);
     console.log(`GHOST > Loaded ${Bot.Commands.Slash.length} slash commands`);
     console.log(`GHOST > Loaded ${Bot.Commands.Plain.length} plain commands`);
 });
 
+// Slash command interaction handler
 Bot.Client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
